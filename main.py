0c9bd8f2a9266dcfac52b1b988f4b0bdd7c9163f
antonismand <antwnhc@hotmail.gr>
2019-02-11 17:45:14 +0200

    categories

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ df = pd.read_csv(filename, sep='\t')
 
 print("There are {} observations and {} features in this dataset. \n".format(df.shape[0], df.shape[1]))
 
+categories = df.Category.unique()
+category = df.groupby("Category")
+
 text = " ".join(article for article in df.Content)
+stopwords = set(STOPWORDS)
 
 # Create and generate a word cloud image:
-wordcloud = WordCloud().generate(text)
+wordcloud = WordCloud(stopwords=stopwords).generate(text)
 
 # Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
